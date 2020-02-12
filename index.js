a1251aaa75a3b61ef5c17c1fa1ef457f5e7fd645
Patrick Nguyen <patrick.nguyen154@gmail.com>
2020-02-12 12:21:40 -0700

    oops

---
+++ index.js
@@ -0,0 +1,15 @@
+function handleThumbnailClicks() {
+  $('.thumbnail').on('click', function(event) {
+    const imgSrc = $(event.currentTarget).find('img').attr('src');  
+    const imgAlt = $(event.currentTarget).find('img').attr('alt');
+      
+    $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+      
+    // Accomplishes the same as line 6 with only one use off .attr()
+    // by passing it an object with multiple properties.
+    // See: http://api.jquery.com/attr/#attr-attributes
+    // $('.hero img').attr({'src': imgSrc, 'alt': imgAlt});
+  });
+}
+  
+$(handleThumbnailClicks);
